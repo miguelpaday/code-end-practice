fad76736ad33b6e70797770cad121a52c2282a23
miguelpaday <miguelpaday12@gmail.com>
2019-11-13 21:54:34 +0400

    This is my first commit

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pubate = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
